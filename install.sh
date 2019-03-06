a9346ddadaed728661ee995af3051143b1d1c68e
Jakub Stefanski <js@jakubstefanski.com>
2019-03-07 00:06:28 +0100

    Add dconf configuration

--- install.sh
+++ install.sh
@@ -7,29 +7,76 @@ fi
 
 set -euo pipefail
 
-declare -a symlinked replaced skipped new
+declare -a matching replaced skipped new
 
 function ensure_link() {
-	local target=${1}
-	local link=${2}
+	local target=${1}       # file in the repository
+	local link=${2}         # destination of the file
+	local id="link ${link}" # id for printing summary
 
 	if [ "${target}" -ef "${link}" ]; then
-		symlinked+=("${link}")
+		matching+=("${id}")
 	elif [ -e "${link}" ]; then
-		read -r -p "replace '${link}'? [y/N] " answer </dev/tty
+		read -r -p "replace ${link} [y/N] " answer </dev/tty
 		case ${answer:0:1} in
 		y | Y | yes | YES)
 			rm -rf "${link}"
 			ln -sf "${target}" "${link}"
-			replaced+=("${link}")
+			replaced+=("${id}")
 			;;
-		*) skipped+=("${link}") ;;
+		*) skipped+=("${id}") ;;
 		esac
 	else
 		mkdir -p "$(dirname "${link}")"
 		ln -sf "${target}" "${link}"
-		new+=("${link}")
+		new+=("${id}")
 	fi
+}
+
+function ensure_dconf() {
+	local confpath=${1}          # for example /org/gnome/shell/extensions/system-monitor/
+	local desiredconf=${2}       # path to desired configuration in dump format
+	local currentconf            # path to a temporary dump of current configuration
+	local id="dconf ${confpath}" # id for printing summary
+	currentconf=$(mktemp)
+
+	# shellcheck disable=SC2064
+	trap "rm -f ${currentconf}" EXIT
+
+	dconf dump "${confpath}" >"${currentconf}"
+	if ! cmp -s "${currentconf}" "${desiredconf}"; then
+		read -r -p "load ${confpath} [y/N] " answer </dev/tty
+		case ${answer:0:1} in
+		y | Y | yes | YES)
+			dconf reset -f "${confpath}"
+			dconf load "${confpath}" <"${desiredconf}"
+			replaced+=("${id}")
+			;;
+		*) skipped+=("${id}") ;;
+		esac
+	else
+		matching+=("${id}")
+	fi
+}
+
+function install_links() {
+	local dir
+	dir="$(dirname "$(realpath "${0}")")"
+
+	while IFS= read -rd '' file; do
+		local relpath=${file#./home/}
+		local target="${dir}/home/${relpath}"
+		local link="${HOME}/.${relpath}"
+		ensure_link "${target}" "${link}"
+	done < <(find "$(dirname "${0}")/home" -type f -print0 | sort -z)
+}
+
+function install_dconf() {
+	while IFS= read -rd '' file; do
+		local relpath=${file#./dconf/}
+		local confpath=/${relpath%.ini}/
+		ensure_dconf "${confpath}" "${file}"
+	done < <(find "$(dirname "${0}")/dconf" -type f -name '*.ini' -print0 | sort -z)
 }
 
 function should_use_colors() {
@@ -75,22 +122,14 @@ function print_summary() {
 	fi
 
 	print_group "${bold}${green}New:${reset}" "${new[@]}"
-	print_group "${bold}${green}Already symlinked:${reset}" "${symlinked[@]}"
+	print_group "${bold}${green}Matching:${reset}" "${matching[@]}"
 	print_group "${bold}${blue}Replaced:${reset}" "${replaced[@]}"
 	print_group "${bold}${red}Skipped:${reset}" "${skipped[@]}"
 }
 
 function main() {
-	local dir
-
-	dir="$(dirname "$(realpath "${0}")")"
-	while IFS= read -rd '' file; do
-		local relpath=${file#./home/}
-		local target="${dir}/home/${relpath}"
-		local link="${HOME}/.${relpath}"
-		ensure_link "${target}" "${link}"
-	done < <(find "$(dirname "${0}")/home" -type f -print0 | sort -z)
-
+	install_links
+	install_dconf
 	print_summary
 }
 
